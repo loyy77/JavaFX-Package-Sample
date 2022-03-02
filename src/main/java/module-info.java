5dd722bc784fb4e8e85c98ff07653de4210af1e2
loyy77 <loyy77@gmail.com>
2022-03-03 07:26:23 +0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module sample {
+
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    requires com.jfoenix;
+
+    exports com.icuxika;
+}
